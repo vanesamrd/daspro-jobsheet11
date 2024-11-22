925c850f48993d954af45744efd99fb92d7a5fa4
vanesamrd <paneja435@gmail.com>
2024-11-22 19:00:45 +0700

    feat modifikasi

--- Kafe23.java
+++ Kafe23.java
@@ -1,6 +1,22 @@
 public class Kafe23 {
 
-    public static void Menu() {
+    //public static void Menu() {
+        //System.out.println("==== MENU RESTO KAFE ====");
+        //System.out.println("1. Kopi Hitam - Rp 15,000");
+        //System.out.println("2. Cappucino - Rp 20,000");
+        //System.out.println("3. Latte - Rp 22,000");
+        //System.out.println("4. Teh Tarik - Rp 12,000");
+        //System.out.println("5. Roti Bakar - Rp 10,000");
+        //System.out.println("6. Mie Goreng - Rp 18,000");
+        //System.out.println("=========================");
+        //System.out.println("Silakan pilih menu yang Anda inginkan");
+    //}
+    //public static void main(String[] args) {
+        //Menu();
+    //}
+
+    //MODIFIKASI SOAL NOMOR 2
+    public static void main(String[] args) {
         System.out.println("==== MENU RESTO KAFE ====");
         System.out.println("1. Kopi Hitam - Rp 15,000");
         System.out.println("2. Cappucino - Rp 20,000");
@@ -10,9 +26,6 @@ public class Kafe23 {
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("=========================");
         System.out.println("Silakan pilih menu yang Anda inginkan");
-    }
-    public static void main(String[] args) {
-        Menu();
     }
 }
 
